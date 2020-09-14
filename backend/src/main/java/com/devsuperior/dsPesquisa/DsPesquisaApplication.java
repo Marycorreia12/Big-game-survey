433afe629feaf9f38e487bf021454019f3acfcfe
Marycorreia12 <correia_nety@hotmail.com>
2020-09-14 15:31:46 +0100

    Project created

---
+++ backend/src/main/java/com/devsuperior/dsPesquisa/DsPesquisaApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsPesquisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsPesquisaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsPesquisaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dsPesquisa/DsPesquisaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsPesquisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsPesquisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
