5115cfd7546f930b3f2656b8a3e372c74b7e3ece
Marycorreia12 <correia_nety@hotmail.com>
2020-09-19 11:48:55 +0100

    Criando pagina de listagem dos registros de pesquisa

--- front-web/src/Routes.tsx
+++ front-web/src/Routes.tsx
@@ -3,18 +3,22 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Header from './Components/Header';
 import Home from './Pages/Home';
 import Records from './Pages/Records';
+import Charts from './Pages/Charts';
 
 const Routes = () => (
     <BrowserRouter>
      <Header />
-     <Switch>
-        <Route path="/" exact>
-          <Home />
-        </Route>
-        <Route path="/records">
-          <Records />
-        </Route>
-    </Switch>
+        <Switch>
+            <Route path="/" exact>
+                <Home />
+            </Route>
+            <Route path="/records">
+                <Records />
+            </Route>
+            <Route path="/charts">
+                <Charts />
+            </Route>
+        </Switch>
     </BrowserRouter>
 );
 
